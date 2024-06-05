24606f829420e32425548f57bae4ac25fa5d0b58
= <=>
2024-06-05 15:00:25 +0500

    init

---
+++ src/test/java/kz/runtime/springexceptionhandlers/SpringExceptionHandlersApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.runtime.springexceptionhandlers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExceptionHandlersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
